539af68c695acb8a451eece28dc25c260f18e6dd
Adrian Quezada <adrianquezada38@gmail.com>
2019-04-06 11:49:14 -0400

    ..

---
+++ app.js
@@ -0,0 +1,28 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBIyoUiv26Uz5dWAPyHwQ-yutIpmhv52Q8",
+    authDomain: "group-projecttt.firebaseapp.com",
+    databaseURL: "https://group-projecttt.firebaseio.com",
+    projectId: "group-projecttt",
+    storageBucket: "group-projecttt.appspot.com",
+    messagingSenderId: "225795243130"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+var name = "";
+var email = "";
+var age = "";
+var comment = "";
+
+$("#add user ").on('click'), function(event) {  
+
+    event.preventDefult();
+    
+
+
+}
+
+
